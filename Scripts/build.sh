ac2380401d6a6a5c4988846e920411908f3ff780
Zach Young <zacharysyoung@gmail.com>
2020-05-04 14:44:35 -0700

    script now works, again

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -14,18 +14,49 @@ export PATH=$Root/bin:$PATH
 # Handy little substitution for splitting PATH into lines
 # echo ${PATH//:/\\n}  # | grep $Root
 
-function download_extract_install() {
+while [ $# -gt 0 ]
+do
+    case "$1" in
+
+    -d)
+        set -x
+        break
+        ;;
+
+    clean)
+        conf=autoconf-2.69
+        make=automake-1.16
+        libt=libtool-2.4.6
+        pkgconf=pkg-config-0.29.2
+
+        cd "$Downloads" || exit 1
+        rm $conf.tar.gz $make.tar.gz $libt.tar.gz $pkgconf.tar.gz
+
+        cd "$Root" || exit 1
+        rm -rf bin include lib share
+
+        cd "$Sources" || exit 1
+        rm -rf $conf $make $libt $pkgconf
+
+        exit 0
+    esac
+
+    shift
+done
+
+
+download_extract_install() {
     _url=$1
     _name=$2
     _targz=$3
-    _flags=$4
-    _ver_pattern=$5
-    _ver_command=$6
+    _ver_pattern=$4
+    _ver_command=$5
+    _config_flags=$6
 
-    s=$(eval "$_ver_command")
+    s=$(eval "$_ver_command 2>&1")
     if [[ $s == *${_ver_pattern}* ]]; then
         print "Skipped $_name, already installed"
-        return
+        return 1
     fi
 
     if [[ -e $Downloads/$_targz ]]; then
@@ -39,7 +70,11 @@ function download_extract_install() {
 
     print -n "Configuring, making, installing $_name..."
     cd "$Sources/$_name" || { echo " Failed to cd to $Sources/$_name"; exit 1; }
-    ./configure --prefix="$Root" "$_flags" >_build.log 2>_error.log
+    if [[ -n $_config_flags ]]; then
+        ./configure --prefix="$Root" "$_config_flags" >_build.log 2>_error.log
+    else
+        ./configure --prefix="$Root" >_build.log 2>_error.log
+    fi
     make >>_build.log 2>>_error.log
     make install >>_build.log 2>>_error.log
     print " done."
@@ -48,71 +83,71 @@ function download_extract_install() {
 # AUTOCONF -- https://www.gnu.org/software/autoconf/
 name=autoconf-2.69
 targz=$name.tar.gz
-flags=" "
 ver_pattern=2.69
 ver_command="$Root/bin/autoconf --version"
+flags=""
 
 args=(
     "http://ftp.gnu.org/gnu/autoconf/$targz"
     "$name"
     "$targz"
-    "$flags"
     "$ver_pattern"
     "$ver_command"
+    "$flags"
 )
-download_extract_install "$args"
 
+download_extract_install $args
 
 # AUTOMAKE -- https://www.gnu.org/software/automake/
 name=automake-1.16
 targz="$name.tar.gz"
-flags=" "
 ver_pattern=1.16
 ver_command="$Root/bin/automake --version"
+flags=""
 
 args=(
     "http://ftp.gnu.org/gnu/automake/$targz"
     "$name"
     "$targz"
-    "$flags"
     "$ver_pattern"
     "$ver_command"
+    "$flags"
 )
-download_extract_install "$args"
+download_extract_install $args
 
 # LIBTOOL -- https://www.gnu.org/software/libtool/
 name=libtool-2.4.6
 targz=$name.tar.gz
-flags=" "
 ver_pattern=2.4.6
 ver_command="$Root/bin/libtool --version"
+flags=""
 
 args=(
     "http://ftp.gnu.org/gnu/libtool/$targz"
     "$name"
     "$targz"
-    "$flags"
     "$ver_pattern"
     "$ver_command"
+    "$flags"
 )
-download_extract_install "$args"
+download_extract_install $args
 
 # PKG-CONFIG -- https://www.freedesktop.org/wiki/Software/pkg-config/
 name=pkg-config-0.29.2
 targz="$name.tar.gz"
-flags="--with-internal-glib"
 ver_pattern=0.29.2
 ver_command="$Root/bin/pkg-config --version"
+flags="--with-internal-glib"
 
 args=(
     "https://pkg-config.freedesktop.org/releases/$targz"
     "$name"
     "$targz"
-    "$flags"
     "$ver_pattern"
     "$ver_command"
+    "$flags"
 )
-download_extract_install
+download_extract_install $args
 
 exit 1
 # ./configure --prefix /Users/kenny/Projects/Tesseract/Root --with-internal-glib
