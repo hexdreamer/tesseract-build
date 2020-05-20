18d2274cd55bec7461760ca30683a900f52c1808
Zach Young <zacharysyoung@gmail.com>
2020-05-20 00:40:51 -0700

    got an arch-specific printout

--- assemble_config.py
+++ assemble_config.py
@@ -3,8 +3,31 @@
 import json
 import sys
 
-with open ('configs.json', 'r') as f:
+from collections import defaultdict
+
+with open('configs.json', 'r') as f:
     config_map = json.loads(f.read())
 
 configs = []
-pkg_name = sys.argv[1]
+pkg = sys.argv[1]
+arch = sys.argv[2]
+
+configs = defaultdict(list)
+for config, pkgs_arches in config_map.items():
+    for pkg_arch in pkgs_arches:
+        if pkg_arch == f'{pkg},{arch}':
+            k, v = config.split(': ')
+            configs[k].append(v)
+
+for k, vs in sorted(configs.items()):
+    # special sort for ./configure to put CC and CCX before --flags
+    vs = sorted(vs, key=lambda x: (x.startswith('-'), x.lower()))
+
+    print('%s=(' % k )
+    for v in vs:
+        if v.startswith('-'):
+            print("'%s'" % v)
+        else:
+            print(v)
+    print(')')
+
--- parse_make.py
+++ parse_make.py
@@ -1,79 +1,83 @@
 #!/usr/bin/env python3
 
-import sys
 import json
+import re
+import sys
+
 from collections import defaultdict
 
+
+def process_lines(lines, config_map):
+    arch = None
+    configs = []
+    pkg = None
+    for line in lines:
+        # Parse exports
+        if line.startswith('export'):
+            name, config = line.split('=', 1)
+            if 'FLAGS' in name and config.startswith('"'):
+                config = config[1:config.rindex('"')]
+
+                if 'CFLAGS' in name:
+                    arch = re.search(r'-arch (.+?) ', config).groups()[0]
+
+                opts = config.split(' -')
+                configs.append(name + ': ' + opts[0])
+                for opt in opts[1:]:
+                    configs.append(name + ': -' + opt)
+            else:
+                configs.append(name + ': ' + config)
+
+        # Parse ./configure
+        if re.match(r'(\.\.\/){1,2}configure', line):
+            configs.append(
+                'configure: ' + line[line.index('CXX'):line.index('CC')-1])
+            configs.append(
+                'configure: ' + line[line.index('CC'):line.index(' --host')])
+
+            other_opts = line[line.index('--host'):]
+            for opt in other_opts.split(' '):
+                configs.append('configure: ' + opt)
+
+        # Get package name
+        if line.startswith('mkdir -p {ROOT}'):
+            line = line.replace('mkdir -p {ROOT}/', '')
+            pkg = line.split('/')[0]
+
+    for config in configs:
+        config = config.replace(pkg, '{PKG_NAME}')
+        config_map[config].add(f'{pkg},{arch}')
+
+    return config_map
+
+
 config_map = defaultdict(set)
-_configs = []
-pkg = None
-platform = None
-scan_for_pkg = False
-target = None
+lines = []
+record_lines=False
 
 with open('make.log', 'r') as f:
     for line in f:
+        if 'usr/bin/make platform=macos' in line:
+            break
+
+        line = line.strip()
         line = line.replace('; \\', '')
         line = line.replace(
             '/Users/zyoung/dev/SwiftyTesseract/SwiftyTesseract/SwiftyTesseract', '{ROOT}')
         line = line.replace(
             '/Applications/Xcode.app/Contents/Developer', '{XCODE_DEV}')
-        line = line.strip()
 
-        if line.contains('usr/bin/make platform='):
-            platform = line.split('=')[1]
+        if line.startswith('export SDKROOT'):
+            record_lines = True
 
-        if line.startswith('curl'):
-            if pkg:
-                for config in _configs:
-                    config = config.replace(pkg, '{PKG_NAME}')
-                    config_map[config].add(pkg)
-                pkg = None
-                _configs = []
+        if record_lines:
+            lines.append(line)
 
-            scan_for_pkg = True
+        if re.match(r'(\.\.\/){1,2}configure', line):
+            config_map = process_lines(lines, config_map)
+            lines=[]
+            record_lines = False
 
-        if line.startswith('export'):
-            name, config = line.split('=', 1)
-            if 'FLAGS' in name and config.startswith('"'):
-                try:
-                    config=config[1:config.rindex('"')]
-                except:
-                    print(line)
-                    sys.exit(1)
-                opts = config.split(' -')
-                _configs.append(name + ': ' + opts[0])
-                for opt in opts[1:]:
-                    _configs.append(name + ': -' + opt)
-            else:
-                _configs.append(name + ': ' + config)
-
-        if '../configure' in line:
-            # "../configure CXX=\"\"\"`xcode-select -p`\"/usr/bin/g++\" --target=x86_64-apple-darwin\" CC=\"\"\"`xcode-select -p`\"/usr/bin/gcc\" --target=x86_64-apple-darwin\" --host=x86_64-apple-darwin --enable-shared=no --prefix=`pwd`":
-            # [
-            #     "libpng-1.6.36",
-            #     "jpeg-9c"
-            # ],
-            _configs.append(
-                'configure: ' + line[line.index('CXX'):line.index('CC')-1])
-            _configs.append(
-                'configure: ' + line[line.index('CC'):line.index(' --host')-1])
-
-            other_opts = line[line.index('--host'):]
-            for opt in other_opts.split(' '):
-                _configs.append('configure: ' + opt)
-
-        if scan_for_pkg and line.startswith('mkdir -p {ROOT}'):
-            # mkdir -p {ROOT}/jpeg-9c/arm-apple-darwin64
-            # extract jpeg-9c
-            line = line.replace('mkdir -p {ROOT}/', '')
-            pkg = line.split('/')[0]
-            # print(pkg)
-            scan_for_pkg = False
-
-for config in _configs:
-    config = config.replace(pkg, '{PKG_NAME}')
-    config_map[config].add(pkg)
 
 for k, v in config_map.items():
     config_map[k] = sorted(list(v))
