36396d676ee8dc5b2f34a6a04b7022345b874f48
Zach Young <zacharysyoung@gmail.com>
2020-06-01 01:25:25 -0700

    added build_pkgconfig

--- Scripts/build/build_pkgconfig.sh
+++ Scripts/build/build_pkgconfig.sh
@@ -4,54 +4,49 @@
 
 scriptname=$0:A
 parentdir=${scriptname%/build_pkgconfig.sh}
-if ! source $parentdir/project_environment.sh; then
-    echo "build_pkgconfig.sh: error sourcing $parentdir/project_environment.sh"
-    exit 1
-fi
-
-if ! source $SCRIPTSDIR/utility.sh; then
-    echo "build_pkgconfig.sh: error sourcing $SCRIPTSDIR/utility.sh"
-    exit 1
+if ! source $parentdir/project_environment.sh -u; then
+  echo "build_pkgconfig.sh: error sourcing $parentdir/project_environment.sh"
+  exit 1
 fi
 
 local name='pkg-config-0.29.2'
+
 print "\n======== $name ========"
 
-# Being respectful of hosts and their bandwidth
+if {
+  [ -f $ROOT/bin/pkg-config ] &&
+    version=$($ROOT/bin/pkg-config --version) &&
+    [[ $version =~ '0.29.2' ]]
+}; then
+  print "Skipped build, found $ROOT/bin/pkg-config w/version 0.29.2"
+  exit 0
+fi
+
+# --  Download / Extract  -----------------------------------------------------
+
 targz=$name.tar.gz
-if [ -e $DOWNLOADS/$targz ]; then
-    echo "Skipped download, using cached $targz in Downloads."
-else
-    print -n 'Downloading...'
-    url="https://pkg-config.freedesktop.org/releases/$targz"
-    xl $name '0_curl' curl -L -f $url --output $DOWNLOADS/$targz
-    print ' done.'
-fi
+url="https://pkg-config.freedesktop.org/releases/$targz"
 
-# Being respectful of any hacking/work done to get a package to build
-if [ -d $SOURCES/$name ]; then
-    echo "Skipped extract of TGZ, using cached $name in Sources."
-else
-    print -n 'Extracting...'
-    xl $name '1_untar' tar -zxf $DOWNLOADS/$targz --directory $SOURCES
-    print ' done.'
-fi
+zsh $parentdir/_download.sh $name $url $targz
+zsh $parentdir/_extract.sh $name $targz
 
-xc mkdir -p $SOURCES/$name/x86_64
-xc cd $SOURCES/$name/x86_64
+# --  Config / Make / Install  ------------------------------------------------
 
-print -n 'x86_64: '
+xc mkdir -p $SOURCES/$name/x86
+xc cd $SOURCES/$name/x86
+
+print -n 'x86: '
 
 print -n 'configuring... '
-export CFLAGS='--target=x86_64-apple-darwin'
-xl $name '2_config_x86_64' ../configure --with-internal-glib --prefix=$ROOT || exit 1
+xl $name '2_config_x86' \
+  ../configure --with-internal-glib "--prefix=$ROOT" || exit 1
 print -n 'done, '
 
 print -n 'making... '
-xl $name '3_clean_x86_64' make clean || exit 1
-xl $name '3_make_x86_64' make || exit 1
+xl $name '3_clean_x86' make clean || exit 1
+xl $name '3_make_x86' make || exit 1
 print -n 'done, '
 
 print -n 'installing... '
-xl $name '4_install_x86_64' make install || exit 1
+xl $name '4_install_x86' make install || exit 1
 print 'done.'
