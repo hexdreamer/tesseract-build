78cada0abf451c5ecb0dc057367ca1ddd7ced1e5
Zach Young <zacharysyoung@gmail.com>
2021-01-20 14:13:41 -0800

    empty project

---
+++ iOCR-iOS/iOCR-iOS/iOCR_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOCR_iOSApp.swift
+//  iOCR-iOS
+//
+//  Created by Zach Young on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct iOCR_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOCR-iOS/iOCR-iOSTests/iOCR_iOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOCR_iOSTests.swift
+//  iOCR-iOSTests
+//
+//  Created by Zach Young on 1/20/21.
+//
+
+import XCTest
+@testable import iOCR_iOS
+
+class iOCR_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
